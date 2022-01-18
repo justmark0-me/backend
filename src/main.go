477ddf04c3dc569d4c57242b7893749f3ddd82ba
Mark Nicholson <wmark.nicholsonu@gmail.com>
2022-01-18 20:31:25 +0300

    ! db connection. Change logging while connectting to db

--- src/main.go
+++ src/main.go
@@ -16,10 +16,10 @@ func main() {
 		os.Getenv("POSTGRES_USER"), os.Getenv("POSTGRES_PASSWORD"), os.Getenv("POSTGRES_HOST"),
 		os.Getenv("POSTGRES_PORT"), os.Getenv("POSTGRES_DBNAME"))
 
-	// Since this logs will be available on server and I were debugging deployment. I decided to log first symbol of password
-	log.Println(fmt.Sprintf("Connecting to postgres. with user %s on host %s with port %s on database %s, password %s",
-		os.Getenv("POSTGRES_USER"), os.Getenv("POSTGRES_HOST"), os.Getenv("POSTGRES_PORT"),
-		os.Getenv("POSTGRES_DBNAME"), os.Getenv("POSTGRES_PASSWORD")))
+	// log.Println(fmt.Sprintf("Connecting to postgres. With user %s on host %s with port %s on database %s",
+	//	os.Getenv("POSTGRES_USER"), os.Getenv("POSTGRES_HOST"), os.Getenv("POSTGRES_PORT"),
+	//	os.Getenv("POSTGRES_DBNAME")))
+	log.Println(fmt.Sprintf("Connecting to postgres. Connection settings: %s", postgresConnectionSettings))
 
 	err := orm.RegisterDataBase("default", "postgres", postgresConnectionSettings)
 	if err != nil {
---
+++ src/services/osType.go
@@ -0,0 +1,6 @@
+package services
+
+// GetOsTypeFromUserAgent returns approximate os type determined based of UserAgent
+func GetOsTypeFromUserAgent(userAgent string) (string, error) {
+	return "", nil
+}
