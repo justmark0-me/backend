0a1dd25cd621e4ed9c97f6d49d02009db216dd2d
Mark Nicholson <wmark.nicholsonu@gmail.com>
2022-01-18 19:46:07 +0300

    ! github ci config. more logging info when app starts

--- src/main.go
+++ src/main.go
@@ -12,14 +12,16 @@ import (
 )
 
 func main() {
-	postgresConfiguration := fmt.Sprintf("user=%s password=%s host=%s port=%s dbname=%s sslmode=disable",
+	postgresConnectionSettings := fmt.Sprintf("user=%s password=%s host=%s port=%s dbname=%s sslmode=disable",
 		os.Getenv("POSTGRES_USER"), os.Getenv("POSTGRES_PASSWORD"), os.Getenv("POSTGRES_HOST"),
 		os.Getenv("POSTGRES_PORT"), os.Getenv("POSTGRES_DBNAME"))
 
-	log.Println(fmt.Sprintf("Connecting to postgres. with user %s on host %s",
-		os.Getenv("POSTGRES_USER"), os.Getenv("POSTGRES_HOST")))
+	// Since this logs will be available on server and I were debugging deployment. I decided to log first symbol of password
+	log.Println(fmt.Sprintf("Connecting to postgres. with user %s on host %s with port %s on database %s, password %s",
+		os.Getenv("POSTGRES_USER"), os.Getenv("POSTGRES_HOST"), os.Getenv("POSTGRES_PORT"),
+		os.Getenv("POSTGRES_DBNAME"), os.Getenv("POSTGRES_PASSWORD")))
 
-	err := orm.RegisterDataBase("default", "postgres", postgresConfiguration)
+	err := orm.RegisterDataBase("default", "postgres", postgresConnectionSettings)
 	if err != nil {
 		log.Fatal("Cannot connect to database.\nQUITTING.\nREASON:", err)
 	}
