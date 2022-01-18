07712c0df20a7dc13f9244591139ee371ecd862d
Mark Nicholson <wmark.nicholsonu@gmail.com>
2022-01-19 00:48:38 +0300

    + cors

--- src/main.go
+++ src/main.go
@@ -7,6 +7,9 @@ import (
 	_ "main/src/routers"
 	"os"
 
+	"github.com/astaxie/beego"
+	"github.com/astaxie/beego/plugins/cors"
+
 	"github.com/beego/beego/v2/client/orm"
 	"github.com/beego/beego/v2/server/web"
 )
@@ -24,5 +27,14 @@ func main() {
 	if err != nil {
 		log.Fatal("Cannot connect to database.\nQUITTING.\nREASON:", err)
 	}
+	// CORS allow all
+	beego.InsertFilter("*", beego.BeforeRouter, cors.Allow(&cors.Options{
+		AllowAllOrigins:  true,
+		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowHeaders:     []string{"Origin", "Authorization", "Access-Control-Allow-Origin", "Access-Control-Allow-Headers", "Content-Type"},
+		ExposeHeaders:    []string{"Content-Length", "Access-Control-Allow-Origin", "Access-Control-Allow-Headers", "Content-Type"},
+		AllowCredentials: true,
+	}))
+
 	web.Run()
 }
